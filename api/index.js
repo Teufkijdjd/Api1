fdbe6b5f29b7d1d52a295fca5ee25dc1612d279f
Neczx <lnwzaaaaaaa55@gmail.com>
2026-03-22 18:00:40 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,12 @@
 module.exports = (req, res) => {
-  const secretKey = "ZeIoNhUb888"; 
+  const secretKey = "ZeIoNhUb888";
 
+  // เช็คว่ามี Token ส่งมาไหม และตรงกับรหัสลับหรือไม่
   if (req.headers['x-auth-token'] === secretKey) {
     res.setHeader('Content-Type', 'text/plain');
-    res.status(200).send(`
-        print("ZeIoNhUb: ระบบทำงาน!")
-        local p = game.Players.LocalPlayer
-        p.Character.Humanoid.WalkSpeed = 100
-    `);
+    res.status(200).send('print("ZeIoNhUb Connected!") game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100');
   } else {
-    // --- บรรทัดเดียวที่คุณต้องการ ---
-    res.status(404).send('404 Not Found'); 
+    // หน้า 404 บรรทัดเดียวที่คุณต้องการ
+    res.status(404).send('404 Not Found');
   }
-};
 };
